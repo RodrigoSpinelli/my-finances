992a0e5ef81d59abe43eb013fd3355569ce6d1a0
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2026-03-25 02:57:34 +0000

    feat: add page transition styles for smoother navigation effects

--- app/app.vue
+++ app/app.vue
@@ -9,3 +9,15 @@ import "vue-sonner/style.css";
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
