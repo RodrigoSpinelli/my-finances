10308114f0acf4d5fb75449783c2af06e18fc87f
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2026-03-25 03:13:26 +0000

    feat: add toggle color mode component for dynamic theme switching

---
+++ app/components/shared/toggle-color-mode/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+</script>
+
+<template>
+  <Button
+    type="button"
+    variant="outline"
+    size="sm"
+    @click="
+      colorMode.preference = colorMode.preference === 'light' ? 'dark' : 'light'
+    "
+  >
+    <Icon name="lucide:sun" v-if="colorMode.value === 'light'" />
+    <Icon name="lucide:moon" v-if="colorMode.value === 'dark'" />
+  </Button>
+</template>
