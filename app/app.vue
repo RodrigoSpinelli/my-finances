4226ed49bb26264993341cddf5df308b7064ebcd
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2026-03-25 01:44:03 +0000

    feat: integrate Toaster component from vue-sonner into app layout

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,11 @@
+<script lang="ts" setup>
+import { Toaster } from "vue-sonner";
+import "vue-sonner/style.css";
+</script>
+
 <template>
   <NuxtLayout>
+    <Toaster position="top-right" :duration="3000" rich-colors expand />
     <NuxtPage />
   </NuxtLayout>
 </template>
