ea49d4a589dea5539ff21f319fda09952f554eca
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2026-03-26 02:09:58 +0000

    feat: add loading indicator to Nuxt layout for improved user experience

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,7 @@ import "vue-sonner/style.css";
 
 <template>
   <NuxtLayout>
+    <NuxtLoadingIndicator :height="4" />
     <Toaster position="top-right" :duration="3000" rich-colors expand />
     <NuxtPage />
   </NuxtLayout>
