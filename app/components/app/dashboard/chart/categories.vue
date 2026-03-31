47b1b0d6f478c27391ee2e27ec5fc58758296217
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2026-03-31 19:30:00 +0100

    feat: enhance category chart component with conditional rendering for empty states and improve code formatting

--- app/components/app/dashboard/chart/categories.vue
+++ app/components/app/dashboard/chart/categories.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import type { Database } from "~/types/database.types"
-import { Progress } from "@/components/ui/progress"
-import { TagIcon } from "lucide-vue-next"
+import type { Database } from "~/types/database.types";
+import { Progress } from "@/components/ui/progress";
+import { TagIcon } from "lucide-vue-next";
 
-type TransactionType = Database["public"]["Enums"]["transaction_type"]
+type TransactionType = Database["public"]["Enums"]["transaction_type"];
 
 interface ItemsData {
-  category_id: string
-  name: string
-  icon: string
-  type: TransactionType
-  color: string
-  color_hex: string
-  transaction_count: number
-  total_amount: number
-  percentage: number
+  category_id: string;
+  name: string;
+  icon: string;
+  type: TransactionType;
+  color: string;
+  color_hex: string;
+  transaction_count: number;
+  total_amount: number;
+  percentage: number;
 }
 
 interface CategoryData {
-  month: string
-  type: TransactionType | "all"
-  items: ItemsData[]
+  month: string;
+  type: TransactionType | "all";
+  items: ItemsData[];
 }
 
 const {
@@ -32,12 +32,15 @@ const {
 
 <template>
   <Card>
-    <CardHeader class="gap-4 flex flex-wrap items-center justify-between border-b">
+    <CardHeader
+      class="gap-4 flex flex-wrap items-center justify-between border-b"
+    >
       <CardTitle>Gastos por categoria</CardTitle>
       <TagIcon />
     </CardHeader>
     <CardContent class="space-y-4">
       <div
+        v-if="categoriesData?.items && categoriesData?.items.length > 0"
         class="grid gap-2"
         v-for="category in categoriesData?.items"
         :key="category.category_id"
@@ -47,6 +50,22 @@ const {
           <span>{{ category.percentage }}%</span>
         </div>
         <Progress :model-value="category.percentage" />
+      </div>
+      <div
+        v-else
+        class="text-muted-foreground flex aspect-square max-h-[250px] flex-col items-center justify-center gap-1 px-4 text-center text-sm"
+      >
+        <p>Nenhuma gasto por categoria encontrada.</p>
+        <p class="text-xs">
+          Registre gastos em
+          <NuxtLink
+            to="/transactions"
+            class="text-foreground underline-offset-4 hover:underline"
+          >
+            transações
+          </NuxtLink>
+          .
+        </p>
       </div>
     </CardContent>
   </Card>
